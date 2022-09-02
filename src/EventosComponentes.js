c974fc06e16ec878080d029f5e4d3df7eb5fc29a
Francisco <franciscoalfar@gmail.com>
2022-09-02 14:32:57 -0400

    nuevos datos

--- src/EventosComponentes.js
+++ src/EventosComponentes.js
@@ -6,6 +6,20 @@ export const EventosComponentes = () => {
         alert("Has dado click "+nombre);
     }
 
+    function hasDadoDobleClick (e){
+      alert("Has dado doble click ");
+  }
+    const hasEntrado = (e, accion)=>{
+      alert(`haz ${accion} a la caja con el mouse`)
+
+    }
+    const estasDentro = (e) =>{
+      console.log("estas dentro del input");
+    }
+    const estasFuera = (e) =>{
+      console.log("estas fuera del input");
+    }
+ 
 
 
   return (
@@ -13,6 +27,24 @@ export const EventosComponentes = () => {
         <h1> Eventos en react</h1>
         {/*evento click*/}
         <button onClick={e=>hasDadoClick(e,"Francisco")}>Has click</button>
+
+        {/*evento doble click*/}
+        <button onDoubleClick={ hasDadoDobleClick }>Dame doble click</button>
+
+
+        <div id="caja" onMouseEnter={ e => hasEntrado(e,"entrado")} onMouseLeave={e => hasEntrado(e,"salido")}>
+          pasa por encima
+
+        </div>
+
+        <p>
+          <input type="text" onFocus={estasDentro} 
+          onBlur ={estasFuera}
+          placeholder="introduce tu nombre"></input>
+        </p>
+
+
+        
     </div>
   )
 }
